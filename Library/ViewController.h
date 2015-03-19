552d0ad730d26686e19ba878d2b1c82bb863b9d1
likedan <likedan5@icloud.com>
2015-03-18 20:23:18 -0500

    Initial Commit

---
+++ Library/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Library
+//
+//  Created by Kedan Li on 15/3/18.
+//  Copyright (c) 2015年 Kedan Li. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Library/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Library
+//
+//  Created by Kedan Li on 15/3/18.
+//  Copyright (c) 2015年 Kedan Li. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
